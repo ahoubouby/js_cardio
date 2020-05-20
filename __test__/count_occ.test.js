94aaab80653962461c6e084fe3e64278771c1eb3
ahoubouby <ahoubouby@gmail.com>
2020-05-20 17:30:39 +0000

    refactoring countOCC test

--- __test__/count_occ.test.js
+++ __test__/count_occ.test.js
@@ -1,22 +1,23 @@
 const { countOcc } = require("../main");
 
 const str = "abdelwahedhoubouby";
+const correctObjString = {
+  a: 2,
+  b: 3,
+  d: 2,
+  e: 2,
+  l: 1,
+  w: 1,
+  h: 2,
+  o: 2,
+  u: 2,
+  y: 1,
+};
 const tab = [1, 2, 2, 1, 3];
 const correctObj = { 1: 2, 2: 2, 3: 1 };
 describe("test count occ", () => {
   it("test occurence with str", () => {
-    expect(countOcc([...str])).toEqual({
-      a: 2,
-      b: 3,
-      d: 2,
-      e: 2,
-      l: 1,
-      w: 1,
-      h: 2,
-      o: 2,
-      u: 2,
-      y: 1
-    });
+    expect(countOcc([...str])).toEqual(correctObjString);
   });
 
   it("test occurence with str", () => {
